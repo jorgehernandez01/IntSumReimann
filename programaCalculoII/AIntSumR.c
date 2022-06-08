e725e6c08a2911ea5ae879e3e70d1bbfa0e66957
jorgehernandez01 <93957851+jorgehernandez01@users.noreply.github.com>
2022-06-07 20:28:36 -0500

    implementación del algoritmo de la suma
    el resultado de la suma se excede al de la integral, por lo que no se aproxima. Esto sucede con la funcion racional f(x)= 1/x.

--- programaCalculoII/AIntSumR.c
+++ programaCalculoII/AIntSumR.c
@@ -1,10 +1,46 @@
 #include <stdio.h>
 #include <math.h>
 
-float funcionR(){
-    //funcion que devolverá la imagen de la funcion racional
+float funcionR(float x){
+    float im;
+
+    im = 1/x;
+
+    return im;
+}
+
+float sumaReimann(){
+    int i, n;
+    float x, f, b, a, acum, Dltx;
+
+    i = 1;
+    acum = 0;
+
+    printf("Digite el intervalo 'a':");
+    scanf("%f",&a);
+    printf("Digite el intervalo 'b':");
+    scanf("%f",&b);
+    printf("Digite el numero de subintervalos:");
+    scanf("%d",&n);
+
+    Dltx = (b - a) / n; //calculo de 'Delta x'
+
+    while(i <= n){
+        x = (Dltx*i*a);
+        f = funcionR(x);
+
+        acum = acum + (f);
+
+        i++;
+    }
+
+    acum = acum * Dltx;
+
+    return acum;
 }
 
 int main(){
-    
+    float resultado = sumaReimann();
+
+    printf("El resultado aproximado es:%0.2f",resultado); //el resultado no es el esperado :(
 }
