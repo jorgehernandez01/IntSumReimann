1d604bf88cc0d7831e56917aa255da514e37e18f
jorgehernandez01 <93957851+jorgehernandez01@users.noreply.github.com>
2022-06-07 19:21:39 -0500

    Update AIntSumR.c

--- programaCalculoII/AIntSumR.c
+++ programaCalculoII/AIntSumR.c
@@ -1,50 +1,10 @@
 #include <stdio.h>
 #include <math.h>
 
-float funcionLineal(){
-    float a, c, imagen;
-
-    printf("Digite un valor para el coeficiente de x:");
-    scanf("%f",&a);
-    printf("Digite un valor para la constante:");
-    scanf("%f",&c);
-
-    imagen = a + c;
-
-    return imagen;
-}
-
-float funcionCuadratica(){
-    float a, b, c, imagen;
-
-    printf("Digite un valor para el coeficiente de x^2:");
-    scanf("%f",&a);
-    printf("Digite un valor para el coeficiente de x:");
-    scanf("%f",&b);
-    printf("Digite un valor para la constante:");
-    scanf("%f",&c);
-
-    imagen = pow(2,a) + b + c;
-
-    return imagen;
+float funcionR(){
+    //funcion que devolverá la imagen de la funcion racional
 }
 
 int main(){
-    short opcion;
-    float im;
-
-    printf("1.- funcion lineal (ax + c). \n 2.- funcion cuadratica (ax^2 + bx + c).\n");
-    printf("Elija la funcion que desee calcular:");
-    scanf("%d",&opcion);
-
-    switch(opcion){
-    case 1:
-        im = funcionLineal();
-        printf("funcion lineal:%0.2f",im);
-        break;
-    case 2:
-        im = funcionLineal();
-        printf("funcion cuadratica:%0.2f",im);
-        break;
-    }
+    
 }
